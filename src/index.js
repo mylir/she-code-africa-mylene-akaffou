5d352131448b18aab965ba8545a07a511c685c7d
mylir <myleneakaffou2@gmail.com>
2022-06-16 14:03:14 +0000

    javascript exercice done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 // complete the function
 function prime (num) {
   // code goes here
-}
-
-function solution (arg) {
-  return prime(arg);
-}
-
-if (typeof require !== "undefined" && require.main === module) {
-  if (process.argv.length <= 2) {
-    throw Error("Require argument");
+  if (num < 2) {
+    return false;
   }
-
-  const arg = process.argv[2];
-  console.log(solution(arg));
+  for (let i = 2; i <= num; i++) {
+    if (num%i == 0) {
+      return false;
+    }else{
+      return true
+    }       
+  }
 }
+console.log(prime(1))
+console.log(prime(4))
+console.log(prime(10))
 
-module.exports = { solution };
+// function solution (arg) {
+//   return prime(arg);
+// }
+
+// if (typeof require !== "undefined" && require.main === module) {
+//   if (process.argv.length <= 2) {
+//     throw Error("Require argument");
+//   }
+
+//   const arg = process.argv[2];
+//   console.log(solution(arg));
+// }
+
+// module.exports = { solution };
